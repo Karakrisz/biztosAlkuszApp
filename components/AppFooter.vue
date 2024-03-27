c98e30ff75740c4c6ecd6144c6c92f916d0e4742
rrd108 <rrd@webmania.cc>
2024-03-27 18:37:24 +0100

    use script setup

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,86 +1,105 @@
-<!-- components/Footer.vue -->
-<template>
-    <footer>
-        <div class="footer-content">
-            <div class="footer-content__form bg-color-w">
-                <h5 class="text-transform-uppercase text-center">VEGYÜK FEL A KAPCSOLATOT!</h5>
-                <div class="contact-form">
-                    <form @submit.prevent="sendEmail">
-                        <div class="contact-form__grid-box grid-2">
-                            <div class="form-group form-group--margin-right">
-                                <input class="form-group__input" placeholder="Vezetéknév *" type="text" id="firstname"
-                                    v-model="form.firstname" required>
-                            </div>
-                            <div class="form-group form-group--margin-left">
-                                <input class="form-group__input" placeholder="Keresztnév *" type="text" id="lastname"
-                                    v-model="form.lastname" required>
-                            </div>
-                        </div>
-                        <div class="contact-form__grid-box grid-2">
-                            <div class="form-group form-group--margin-right">
-                                <input class="form-group__input" placeholder="E-mail cím *" type="email" id="email"
-                                    v-model="form.email" required>
-                            </div>
-                            <div class="form-group form-group--margin-left">
-                                <input class="form-group__input" placeholder="Telefonszám" type="text" id="phonenumber"
-                                    v-model="form.phonenumber" required>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-group__textarea" placeholder="Milyen segítségre volna szüksége?"
-                                id="message" v-model="form.message" required></textarea>
-                        </div>
-                        <div class="contact-form__btn-box t-end">
-                            <button class="page-btn text-color-w" type="submit">Küldés
-                                <img class="page-btn__img position-relative" src="/img/btn-arrow.svg"
-                                    alt="Biztos Alkuszom"></button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="footer-content__link-box text-center">
-                <img src="/img/footer/logo.svg" alt="Biztos Alkuszom" class="footer-content__link-box__img">
-                <div class="footer-content__link-box__links">
-                    <NuxtLink v-for="link in footerLinks" :key="link.path" :to="link.path" class="footer-link">
-                        {{ link.name }}
-                    </NuxtLink>
-                </div>
-                <div class="footer-content__link-box__DigitalSeed bg-color-w">
-                    <NuxtLink class="footer-content__link-box__DigitalSeed__Nuxtlink text-transform-uppercase" to="/">
-                        DIGITAL SEED STUDIO - 2024</NuxtLink>
-                </div>
-            </div>
-        </div>
-    </footer>
-</template>
+<script setup>
+  const links = [
+    { name: 'Főoldal', path: '/' },
+    { name: 'Partnerek', path: '/partnerek' },
+    { name: 'Szolgáltatások', path: '/szolgaltatasok' },
+    { name: 'Dokumentumok', path: '/dokumentumok' },
+    { name: 'Ajánlatkérés', path: '/ajanlatkeres' },
+    { name: 'Kapcsolat', path: '/kapcsolat' },
+  ]
 
+  const form = ref({
+    firstname: '',
+    lastname: '',
+    email: '',
+    phonenumber: '',
+    message: '',
+  })
 
-<script>
-import axios from 'axios';
-import { Footerlinks } from '~/utils/js/links';
-
-export default {
-    data() {
-        return {
-            footerLinks: Footerlinks,
-            form: {
-                firstname: '',
-                lastname: '',
-                email: '',
-                phonenumber: '',
-                message: ''
-            }
-        };
-    },
-    methods: {
-        async sendEmail() {
-            try {
-                await axios.post('/send-email', this.form);
-                console.log('Email sikeresen elküldve');
-            } catch (error) {
-                console.error('Hiba történt az email küldésekor:', error);
-            }
-        }
-    }
-};
+  const sendEmail = () => console.log(form.value)
 </script>
+
+<template>
+  <footer>
+    <div class="footer-content">
+      <div class="footer-content__form bg-color-w">
+        <h5 class="text-transform-uppercase text-center">VEGYÜK FEL A KAPCSOLATOT!</h5>
+        <div class="contact-form">
+          <form @submit.prevent="sendEmail">
+            <div class="contact-form__grid-box grid-2">
+              <div class="form-group form-group--margin-right">
+                <input
+                  class="form-group__input"
+                  placeholder="Vezetéknév *"
+                  type="text"
+                  id="firstname"
+                  v-model="form.firstname"
+                  required
+                />
+              </div>
+              <div class="form-group form-group--margin-left">
+                <input
+                  class="form-group__input"
+                  placeholder="Keresztnév *"
+                  type="text"
+                  id="lastname"
+                  v-model="form.lastname"
+                  required
+                />
+              </div>
+            </div>
+            <div class="contact-form__grid-box grid-2">
+              <div class="form-group form-group--margin-right">
+                <input
+                  class="form-group__input"
+                  placeholder="E-mail cím *"
+                  type="email"
+                  id="email"
+                  v-model="form.email"
+                  required
+                />
+              </div>
+              <div class="form-group form-group--margin-left">
+                <input
+                  class="form-group__input"
+                  placeholder="Telefonszám"
+                  type="text"
+                  id="phonenumber"
+                  v-model="form.phonenumber"
+                  required
+                />
+              </div>
+            </div>
+            <div class="form-group">
+              <textarea
+                class="form-group__textarea"
+                placeholder="Milyen segítségre volna szüksége?"
+                id="message"
+                v-model="form.message"
+                required
+              ></textarea>
+            </div>
+            <div class="contact-form__btn-box t-end">
+              <button class="page-btn text-color-w" type="submit">
+                Küldés <img class="page-btn__img position-relative" src="/img/btn-arrow.svg" alt="Biztos Alkuszom" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="footer-content__link-box text-center">
+        <img src="/img/footer/logo.svg" alt="Biztos Alkuszom" class="footer-content__link-box__img" />
+        <div class="footer-content__link-box__links">
+          <NuxtLink v-for="link in links" :key="link.path" :to="link.path" class="footer-link">
+            {{ link.name }}
+          </NuxtLink>
+        </div>
+        <div class="footer-content__link-box__DigitalSeed bg-color-w">
+          <NuxtLink class="footer-content__link-box__DigitalSeed__Nuxtlink text-transform-uppercase" to="/">
+            DIGITAL SEED STUDIO - 2024</NuxtLink
+          >
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
